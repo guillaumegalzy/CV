1f8343c12628dbe9e158815c0bf9d4450b16f05c
guillaume galzy <guillaume.galzy@hotmail.fr>
2020-09-20 19:24:16 +0200

    Poursuite travail sur navbar + Footer

--- js/bin/script.js
+++ js/bin/script.js
@@ -1,9 +1,18 @@
-$(document).ready(function(){
-  $('#navbar').pushpin({
-    top: $('#navbar').offset().top
+/* Initialisation fonction JS via JQUERY */
+
+  /* Inititialisation Puspin - Sticky navbar */
+  $(document).ready(function(){
+    $('#navbar').pushpin({
+      top: $('#navbar').offset().top
+    });
+
+    $('#Contact').pushpin({
+      bottom: $('#Contact').offset().bottom
+    });
+
+    /* Inititialisation Scrossspy  - Suivi scrolling page*/
+    $('.scrollspy').scrollSpy({
+      scrollOfffset: 0,
+      activeClass:'active'
+    });
   });
-  $('.scrollspy').scrollSpy({
-    scrollOfffset: 0,
-    activeClass:'active'
-  });
-});
