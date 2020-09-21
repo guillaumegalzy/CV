3f222006084f251051801b1dc353981b52e63445
guillaume GALZY <guillaume.galzy@hotmail.fr>
2020-09-21 15:46:49 +0200

    Mise en place Footer fixe avec scroll Début mise en page des sections Début rating des connaissances

--- js/bin/script.js
+++ js/bin/script.js
@@ -1,18 +1,31 @@
-/* Initialisation fonction JS via JQUERY */
+/* Initialisation JQUERY */
+$(document).ready(function(){
 
+/* Initialisation fonction JS via JQUERY */
   /* Inititialisation Puspin - Sticky navbar */
-  $(document).ready(function(){
     $('#navbar').pushpin({
       top: $('#navbar').offset().top
     });
 
-    $('#Contact').pushpin({
-      bottom: $('#Contact').offset().bottom
-    });
-
-    /* Inititialisation Scrossspy  - Suivi scrolling page*/
+    /* Inititialisation Scrollspy  - Suivi scrolling page*/
     $('.scrollspy').scrollSpy({
-      scrollOfffset: 0,
+      scrollOfffset:0,
       activeClass:'active'
     });
   });
+
+  /* Ajout trigger pour passer le footer en position:fixed à partir d'un certain défilement*/
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop(); //récupère avancement scroll de la page
+    
+    var headerHeight=$('header').height(); //récupère taille du header scroll de la page
+  
+    if(scroll >= headerHeight){ /* Passage en fixed à partir du moment ou le scroll vertical est supérieur à celui du header */
+      $('#Contact').addClass("fixed-footer");
+      $('#Contact').fadeIn(1000); //délai en ms pour l'animation
+      
+    } else {
+      $('#Contact').fadeOut(1000);
+      $('#Contact').removeClass("fixed-footer");
+    }
+});
