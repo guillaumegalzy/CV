024acfce47fd5bde562adf4d80fbb71313327d5e
guillaume galzy <guillaume.galzy@hotmail.fr>
2020-10-05 19:21:54 +0200

    Correction taille footer pour correspondre à celle de la navbar sur mobile Intégration bouton Back to top au footer Footer fully responsive Réorganisation du footer Ajout et modification icone '+' et 'X' des cards de la section 'Compétences' Ajout overflow-hidden sur html pour éviter scroll horizontal inutile

--- js/bin/script.js
+++ js/bin/script.js
@@ -17,19 +17,19 @@ $(document).ready(function(){
     $('.tooltipped').tooltip({
       inDuration: 700, // temps de l'animation en ms
       outDuration: 500, // temps de l'animation en ms
-      margin: 40, // margin par rapport à son élément de réfénce
-      transitionMovement: 40, // déplacement vertical du tooltip lors de son animation
-      exitDelay: 100
+      margin: 0, // margin par rapport à son élément de référence
+      transitionMovement: 100, // déplacement vertical du tooltip lors de son animation
+      exitDelay: 200
     });
   });
 
-
-  /* Ajout trigger pour passer le footer en position:fixed à partir d'un certain défilement*/
+/* Ajout des triggers selon la position de la page */
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop(); //récupère avancement scroll de la page
     
     var headerHeight=$('header').height(); //récupère taille du header scroll de la page
-  
+
+    /* Ajout trigger pour passer le footer en position:fixed à partir d'un certain défilement*/
     if(scroll >= headerHeight){ /* Passage en fixed à partir du moment ou le scroll vertical est supérieur à celui du header */
       $('footer').addClass("fixed-footer");
       $('footer').fadeIn(800); //délai en ms pour l'animation
